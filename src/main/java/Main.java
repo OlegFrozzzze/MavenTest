2c42aeb5bd3738f561c687954d553e9f44502f6f
Frozzzze <ji900@yandex.ru>
2017-09-07 18:18:47 +0300

    Exception checking

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,8 @@
-
+import TestThrows.Developer;
 
 public class Main {
     public static void main(String[] args) throws Exception{
-
+        Developer developer = new Developer();
+        developer.developerJava();
     }
 }
