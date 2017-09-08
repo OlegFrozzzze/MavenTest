c7f3dba6576307651148153ca8fd48f011c8e87f
Frozzzze <ji900@yandex.ru>
2017-09-08 20:38:27 +0300

    Created a new class Fronted extends HttpServlet

---
+++ src/main/java/Servlet/Frontend.java
@@ -0,0 +1,22 @@
+package Servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class Frontend extends HttpServlet{
+    private String login = " ";
+
+    public void doGet(HttpServletRequest request,
+                      HttpServletRequest response)
+        throws ServletException, IOException{
+
+    }
+
+        public void doPost(HttpServletRequest request,
+                           HttpServletRequest response)
+                throws ServletException, IOException{
+
+        }
+}
